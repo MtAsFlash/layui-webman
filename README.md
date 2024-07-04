fe4b13288854d5f5c37a978565628e74194b6f17
mtasflash <mtasflash@163.com>
2024-07-04 18:29:45 +0800

    项目初始化:webman基础代码

---
+++ README.md
@@ -0,0 +1,3 @@
+> 基于[Webman](https://github.com/webman-php/webman)开发
+>
+> 开发参考项目 https://gitee.com/xianrenqh/huicmf_webman.git
---
+++ config/middleware.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+return [
+    'api' => [
+//        \app\api\middleware\AccessControl::class,
+//        \app\api\middleware\AuthCheckAccess::class,
+    ],
+    'admin' => [
+//        \plugin\admin\app\middleware\AccessControl::class,
+//        \plugin\admin\app\middleware\SystemLogControl::class,
+//        \plugin\admin\app\middleware\Lang::class,
+    ],
+];
